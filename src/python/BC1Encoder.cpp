19028db20906e3252db2fed3284cf4f408244a6a
drewcassidy <drewcassidy@me.com>
2021-03-04 02:21:16 -0800

    Finally start testing pybind stuff

---
+++ src/python/BC1Encoder.cpp
@@ -0,0 +1,30 @@
+/*  Python-rgbcx Texture Compression Library
+    Copyright (C) 2021 Andrew Cassidy <drewcassidy@me.com>
+    Partially derived from rgbcx.h written by Richard Geldreich <richgel99@gmail.com>
+    and licenced under the public domain
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <pybind11/pybind11.h>
+#include "../BC1/BC1Encoder.h"
+
+#define STRINGIFY(x) #x
+#define MACRO_STRINGIFY(x) STRINGIFY(x)
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(python_rgbcx, m) {
+    m.doc() = "More Stuff";
+}
---
+++ tools/install.fish
@@ -0,0 +1,6 @@
+#! /usr/bin/bash
+
+set dir (status dirname)
+
+source "$dir"/../env/bin/activate.fish
+pip install "$dir"/..
