2a9dbe6a92db97d66fbc601faa9dac88d7ef92c7
drewcassidy <drewcassidy@me.com>
2021-03-19 14:36:45 -0700

    I'm so confused part 3

--- docs/conf.py
+++ docs/conf.py
@@ -27,9 +27,9 @@ author = 'Andrew Cassidy'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    # 'myst_parser',
-    # 'sphinx_rtd_theme',
-    'sphinx.ext.autodoc'
+    'myst_parser',
+    'sphinx_rtd_theme',
+    'sphinx.ext.autodoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -45,9 +45,17 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# -- Options for Autodoc -----------------------------------------------------
+add_module_names = False
+
+autodoc_default_options = {
+    'member-order': 'groupwise',
+    'exclude-members': '__weakref__'
+}
