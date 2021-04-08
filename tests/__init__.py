d6b8a18e6fa6db4bac32821f944df0485bbc9e84
drewcassidy <drewcassidy@me.com>
2021-04-07 19:10:22 -0700

    add submodule tests to make sure everything is present before proceeding

---
+++ tests/__init__.py
@@ -0,0 +1,19 @@
+import unittest
+import nose.tools
+import os.path
+
+tests_path = os.path.dirname(os.path.realpath(__file__))
+
+
+class TestInstall(unittest.TestCase):
+    """Test if everything is installed correctly"""
+
+    def test_images(self):
+        """Test for the images submodule"""
+
+        images_path = os.path.join(tests_path, 'images')
+
+        assert os.path.isdir(images_path), 'images directory/submodule not present'
+        assert os.path.isfile(os.path.join(images_path, '__init__.py')), 'images __init__.py not present, is the submodule checked out?'
+        bp_size = os.path.getsize(os.path.join(images_path, 'Boilerplate.png'))
+        assert bp_size == 955989, 'Boilerplate.png is the wrong size, is the submodule checked out with LFS enabled?'
